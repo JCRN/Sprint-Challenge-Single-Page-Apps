a0fa89428675935efa8aead8463198b93fb93155
JCRN <clemons.jason@gmail.com>
2019-07-26 08:25:27 -0700

    WIP: character card - added imports and state

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [chararcters, setCharacters] = useState();
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+  }, []);
 
-  return <section className='character-list grid-view'>
-
+  return (
+    <section className="character-list grid-view">
       <h2>TODO: `array.map()` over your state here!</h2>
     </section>
-
+  );
 }
