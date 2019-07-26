4efa5d576d7a834d3f99b12149b29573bff345f7
JCRN <clemons.jason@gmail.com>
2019-07-26 10:12:12 -0700

    Part 1 Completed

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Card, Label } from "semantic-ui-react";
 
-export default function LocationCard ({ name, type, dimension, residents }) {
+export default function LocationCard({ name, type, dimension, residents }) {
   // image={image}
-  return (<span>todo: location</span>
-  )
+  return (
+    <Card>
+      <Card.Content header={(name, type)} />
+      <Card.Content description={dimension} />
+      <Card.Content extra>
+        <Label attached="bottom right">{residents} residents</Label>
+      </Card.Content>
+    </Card>
+  );
 }
