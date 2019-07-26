943da4fbcd1e691a2b3552a43272804ad56ee039
JCRN <clemons.jason@gmail.com>
2019-07-26 09:33:35 -0700

    completed character card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard({ character }) {
+  return (
+    <Card>
+      <Image src={character.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{character.name}</Card.Header>
+        <Card.Meta>
+          <span className="character-species">{character.species}</span>
+          <span className="character-status">{character.status}</span>
+        </Card.Meta>
+        <Card.Description>
+          <span className="charachter-location">
+            Location: {character.location.name}
+          </span>
+          <span className="character-origin">
+            Origin: {character.origin.name}
+          </span>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          Episodes
+        </a>
+      </Card.Content>
+    </Card>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,24 +3,28 @@ import axios from "axios";
 import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  const [chararcters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
       .get("https://rickandmortyapi.com/api/character/")
       .then(response => {
-        setCharacters(response.data);
+        setCharacters(response.data.results);
       })
       .catch(error => {
         console.error("Server Error", error);
       });
   }, []);
 
+  console.log("characters", characters);
+
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>
+        {characters.map(character => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
+      </h2>
     </section>
   );
 }
