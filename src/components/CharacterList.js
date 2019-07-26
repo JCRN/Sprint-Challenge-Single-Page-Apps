1595136444881fe5a7a74fe1b3c5d08c870c063e
JCRN <clemons.jason@gmail.com>
2019-07-26 08:29:49 -0700

    WIP: character card - added axios call

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,11 +3,19 @@ import axios from "axios";
 import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  const [chararcters, setCharacters] = useState();
+  const [chararcters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        setCharacters(response.data);
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
   }, []);
 
   return (
